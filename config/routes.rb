b3a5053c22e7093828661b9d91eaede2a9dac777
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-20 20:01:08 +0100

    Add user sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "user#index"
+  root to: "users#index"
   resources :group_operations
   resources :groups
-  resources :operations
+  get 'users/index', to: 'users#index'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   resources :users
+  resources :operations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
