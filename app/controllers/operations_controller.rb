bb2667240f37a2e0fce0b2f94799744751123889
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-22 12:45:57 +0100

    Add categories to add transaction page

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -3,7 +3,7 @@ class OperationsController < ApplicationController
   before_action :authenticate_user!
   def index
     @group = current_user.groups.find(params[:group_id])
-    @operations = @group.operations
+    @operations = @group.operations.order(created_at: :desc)
   end
 
   def new
